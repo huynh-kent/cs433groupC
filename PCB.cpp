6d045e8e215bf5e4e0035b5fef8308270d02d23a
Shoko Aoi <56996265+dj-shoko@users.noreply.github.com>
2022-02-10 10:08:41 -0800

    Update PCB.cpp
    Added constructor and change state methods.

--- PCB.cpp
+++ PCB.cpp
@@ -1,4 +1,35 @@
+using namespace std;
 #include "PCB.h"
 
-
 // TODO: Add your implementation here
+
+// Constructor
+PCB::PCB(x, p) {
+  ProcState = NEW; //initialize the process state as NEW
+  id = x;
+  priority = p;
+}
+
+// Destructor
+PCB::~PCB() {
+}
+
+// Sets PCB state as READY
+void PCB::stateReady() {
+  ProcState = READY;
+}
+
+// Sets PCB state as RUNNING
+void PCB::stateRunning() {
+  ProcState = RUNNING;
+}
+
+// Sets PCB state as WAITING
+void PCB::stateWaiting() {
+  ProcState = WAITING;
+}
+
+// Sets PCB state as TERMINATED
+void PCB::stateTerminated() {
+  ProcState = TERMINATED;
+}
