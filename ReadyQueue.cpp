f36afbd87a5053780dfe6f74c2e82e6edc194b6e
Shoko Aoi <56996265+dj-shoko@users.noreply.github.com>
2022-02-14 09:17:54 -0800

    Update ReadyQueue.cpp
    Added function formatting

--- ReadyQueue.cpp
+++ ReadyQueue.cpp
@@ -1,4 +1,26 @@
 #include <iostream>
 #include "ReadyQueue.h"
+#include <PCB.h>
 
 // TODO: Add your implementation of ReadyQueue functions here
+// Add new PCB to the queue
+void ReadyQueue::addPCB(PCB* pcbPtr) {
+  
+}
+
+// Remove and return PCB with highest queue priority
+void ReadyQueue::PCB* removePCB() {
+  
+}
+
+// Return the queue size
+int ReadyQueue::size() {
+  return queueSize;
+}
+
+// Displays content of the Queue
+void ReadyQueue::display() {
+  for (int i = 0; i < size(); i++) {
+    //cout << returnID() << "\t" << returnPriority() << endl;
+  }
+}
