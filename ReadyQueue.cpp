537509c662b2ea1427ae8687e86e33626a928ef5
kentyboy2 <78402151+kentyboy2@users.noreply.github.com>
2022-02-14 21:55:05 -0800

    Update ReadyQueue.cpp
    added constructor, functions for add, remove, sort PCB/queue

--- ReadyQueue.cpp
+++ ReadyQueue.cpp
@@ -1,16 +1,67 @@
 #include <iostream>
 #include "ReadyQueue.h"
-#include <PCB.h>
 
 // TODO: Add your implementation of ReadyQueue functions here
+
+// constructor build an array queue with given size
+ReadyQueue::ReadyQueue(int size) {
+    queueSize = -1;
+    arraySize = size;
+    arr = new int[size];
+}
+
+// deconstructor
+ReadyQueue::~ReadyQueue(){
+    
+}
+
+// swaps two pcbs
+void swapPCBs(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 // Add new PCB to the queue
-void ReadyQueue::addPCB(PCB* pcbPtr) {
-  
+void ReadyQueue::addPCB(int k) {
+    
+    // insert new pcb at the end of queue
+    int i = queueSize - 1;
+    arr[i] = k;
+    queueSize++;
+    
+    //sort if priority is higher than parent pcb
+    while (i > 0 && arr[i] > arr[(i-1)/2]) {
+        swapPCBs(&arr[i], &arr[(i-1)/2]);
+        i = (i-1)/2;
+    }
 }
 
 // Remove and return PCB with highest queue priority
-void ReadyQueue::PCB* removePCB() {
-  
+int ReadyQueue::removePCB(){
+  // return max priority pcb and remove from queue
+  int maxPriority = arr[0];
+  arr[0] = arr[queueSize - 1];
+  queueSize--;
+  sortQueue(0);
+  return maxPriority;
+}
+
+// recursive sorting starting from given index i
+void ReadyQueue::sortQueue(int i){
+    int higher = i;
+    int left = (2*i + 1);
+    int right = (2*i + 2);
+    if (left < queueSize && arr[left] > arr[i])
+        higher = left;
+    if (right < queueSize && arr[right] > arr[i])
+        higher = right;
+
+    if (higher != i) {
+        swapPCBs(&arr[i], &arr[higher]);
+        sortQueue(higher);
+    }
+
 }
 
 // Return the queue size
