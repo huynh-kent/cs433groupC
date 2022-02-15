3e65cb8f5e9486b5517301e4892ecf68c6f57c20
kentyboy2 <78402151+kentyboy2@users.noreply.github.com>
2022-02-15 07:10:43 -0800

    Update ReadyQueue.cpp

--- ReadyQueue.cpp
+++ ReadyQueue.cpp
@@ -20,38 +20,42 @@ void swapPCBs(PCB *a, PCB *b) {
     *b = temp;
 }
 
-// Add new PCB to the queue
+// Add new PCB to the queue 
 void ReadyQueue::addPCB(PCB p) {
-    
+    //set state to ready
+    p.setReady();
+
     // insert new pcb at the end of queue
     queueSize++;
     int i = queueSize - 1;
     pcbArray[i] = p;
-    
+
     //sort if priority is higher than parent pcb
     while (i > 0 && pcbArray[i].priority > pcbArray[(i-1)/2].priority) {
         swapPCBs(&pcbArray[i], &pcbArray[(i-1)/2]);
         i = (i-1)/2;
-    }
+        }
 }
 
-// Remove and return PCB with highest queue priority
+// Remove and run PCB with highest queue priority
 void ReadyQueue::removePCB(){
-  // return max priority pcb and remove from queue then resort queue
+    //set state to running
+    pcbArray[0].setRunning();
+  // remove max priority pcb and remove from queue then resort queue
   //PCB maxPriority = pcbArray[0];
   pcbArray[0] = pcbArray[queueSize - 1];
   queueSize--;
   sortQueue(0);
 }
 
-// recursive sorting starting from given index i
+// recursive sorting starting from given index i move highest priority to the top
 void ReadyQueue::sortQueue(int i){
     int higher = i;
     int left = (2*i + 1);
     int right = (2*i + 2);
     if (left < queueSize && pcbArray[left].priority > pcbArray[i].priority)
         higher = left;
-    if (right < queueSize && pcbArray[right].priority > pcbArray[i].priority)
+    if (right < queueSize && pcbArray[right].priority > pcbArray[higher].priority)
         higher = right;
 
     if (higher != i) {
@@ -68,7 +72,8 @@ int ReadyQueue::size() {
 
 // Displays content of the Queue
 void ReadyQueue::display() {
-   cout << "PCB queue: \n___________________________________\n" 
+if (size() == 0) { cout << "ReadyQueue is Now Empty\n"; return; }
+   cout << "\nPCB queue: \n___________________________________\n" 
    << "ID \t Priority \t State" << endl;
   for (int i = 0; i < size(); i++) {
     cout << pcbArray[i].id << "\t" << pcbArray[i].priority << "\t" << pcbArray[i].state << endl;
