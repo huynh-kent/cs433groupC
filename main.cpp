0bc201b24d95b1c3ae972ea57b7f3c7198e2af8d
Daniel Blaine Martinez <57377837+dbmartinez@users.noreply.github.com>
2022-02-14 23:20:02 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 int main(int argc, char* argv[]) {
 	//Print basic information about the program
 	std::cout << "CS 433 Programming assignment 1" << std::endl;
-	std::cout << "Author: Kent Huynh and xxxxxxx" << std::endl;
+	std::cout << "Author: Kent Huynh, Daniel Martinez, and Raymond Quach" << std::endl;
 	std::cout << "Date: 02/09/2022" << std::endl;
 	std::cout << "Course: CS433 (Operating Systems)" << std::endl;
 	std::cout << "Description : Program to implement a priority ready queue of processes" << std::endl;
@@ -16,13 +16,21 @@ int main(int argc, char* argv[]) {
 
 	// TODO: Add your code for Test 1
 	std::cout << "Performing Test 1" << std::endl;
+	
+	// Call readyQueue
+	ReadyQueue q1, q2;
+	
 	//TODO: add processes 14, 1, 28, 39 and 18 to q1. Display the content of q1
 	
 	//TODO: remove one process with the highest priority from q1 and display q1.
+	q1.remove();
+	q1.display();
 	
 	//TODO: add additional processes 48, 10, 31 and 19 into q1, and display q1.
 	
 	//TODO: remove one process with the highest priority from q1 and display q1.
+	q1.remove();
+	q1.display();
 	
 	//TODO: add additional processes 29, 41, 18 and 46 to q1 and display q1.
 	
